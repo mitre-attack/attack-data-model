91b8f0ae091ea036de9cfe44951903b124bc4a68
Jared Ondricek <jondricek@mitre.org>
2024-02-14 09:27:14 -0600

    Move stuff around

--- validate-attack-stix-bundles.py
+++ validate-attack-stix-bundles.py
@@ -9,23 +9,21 @@ def main():
     cwd = Path.cwd()
 
     # Access the appropriate bundle
-    file = cwd / "cti/enterprise-attack/enterprise-attack.json"
-    # for group_bundle_file in groups_dir.iterdir():
-    f = open(file, "r")
-    attack_bundle = json.load(f)
+    file = cwd / "attack-releases/stix-2.0/v14.1/enterprise-attack.json"
+
+    with open(file, "r") as f:
+        attack_bundle = json.load(f)
 
     # Allow jsonschema validator to read from local directories
-    path = Path.cwd()
     resolver = jsonschema.validators.RefResolver(
-        base_uri=f"{path.as_uri()}/",
+        base_uri=f"{cwd.as_uri()}/",
         referrer=True,
     )
 
     try:
         jsonschema.validate(
             instance=attack_bundle,
-            # schema={"$ref": "schema/json/attack-group.schema.json"},
-            schema={"$ref": "attack-data-model/schema/json/attack-domain-bundle.schema.json"},
+            schema={"$ref": "schema/json/attack-domain-bundle.schema.json"},
             resolver=resolver,
         )
         print("SUCCESS: "+f"{file}")
