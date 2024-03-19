29294f47bf64819086d262fc8e767db4294436b3
Sean Sica <23294618+seansica@users.noreply.github.com>
2024-03-19 13:44:27 -0400

    Refactor STIXBoolean class to support bool conversion

--- attack_model/models/common/boolean.py
+++ attack_model/models/common/boolean.py
@@ -1,7 +1,9 @@
 from pydantic import ValidationInfo
+from typing import Annotated
+from pydantic.functional_serializers import PlainSerializer
 
 
-class STIXBoolean(str):
+class _STIXBoolean(int):
     """
     Custom field type representing a boolean value stored as a lowercase string in ATT&CK/STIX objects.
     """
@@ -11,7 +13,20 @@ class STIXBoolean(str):
         yield cls.validate
 
     @classmethod
-    def validate(cls, value, field: ValidationInfo):
+    def validate(cls, value, field: ValidationInfo = None):
+        if isinstance(value, bool):
+            return int(value)
+
         if value not in ["true", "false"]:
             raise ValueError('STIXBoolean must be "true" or "false"')
         return value == "true"
+
+    @classmethod
+    def bool_to_stix_boolean_str(cls, value: bool) -> str:
+        return str(value).lower()
+
+
+STIXBoolean = Annotated[
+    _STIXBoolean,
+    PlainSerializer(lambda x: _STIXBoolean.bool_to_stix_boolean_str(bool(x)), return_type=str),
+]
