4ccef67e74ac9f2bb70dafda38ecfc8107971b5f
adpare <apare@mitre.org>
2024-07-16 23:11:17 -0400

    added tests for malware, tool, identity

---
+++ zod/test/sdo.identity.test.ts
@@ -0,0 +1,47 @@
+import { SDOSchema } from '../src/schemas/common/core-stix-sdo.schema';
+import { IdentitySchema } from "../src/schemas/sdo/identity.schema";
+
+describe('IdentitySchema', () => {
+  // Test valid object
+  test('should validate a correct Identity object', () => {
+    const validObject = {
+      type: 'identity',
+      id: 'identity--c78cb6e5-0c4b-4611-8297-d1b8b55e40b5',
+      created: "2017-06-01T00:00:00.000Z",
+      modified: "2017-06-01T00:00:00.000Z",
+      object_marking_refs: [
+          "marking-definition--fa42a846-8d90-4e51-bc29-71d5b4802168"
+      ],
+      identity_class: "organization",
+      name: "The MITRE Corporation"
+    };
+    expect(() => IdentitySchema.parse(validObject)).not.toThrow();
+  });
+
+  // Test required fields
+  test('should throw error when required fields are missing', () => {
+    const invalidObject = {
+      type: 'identity',
+      id: 'identity--c78cb6e5-0c4b-4611-8297-d1b8b55e40b5',
+      created: "2017-06-01T00:00:00.000Z",
+      modified: "2017-06-01T00:00:00.000Z",
+      object_marking_refs: [
+          "marking-definition--fa42a846-8d90-4e51-bc29-71d5b4802168"
+      ],
+      name: "The MITRE Corporation"
+    };
+    expect(() => IdentitySchema.parse(invalidObject)).toThrow();
+  });
+
+  // Test invalid ID format
+  test('should throw error when ID format is invalid', () => {
+    const objectWithInvalidId = {
+      id: 'tool--11f8d7eb-1927-4806-9267-3a11d4d4d6be',
+      type: 'identity',
+      spec_version: '2.1',
+      created: '2023-06-22T10:00:00.000Z',
+      modified: '2023-06-22T10:00:00.000Z'
+    };
+    expect(() => IdentitySchema.parse(objectWithInvalidId)).toThrow();
+  });
+});
