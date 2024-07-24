f356cb814d7da950191b6cc36009ef18bac533dd
Sean Sica <23294618+seansica@users.noreply.github.com>
2024-07-24 15:14:24 -0400

    Add Zod enum for the identity-class-ov vocabulary

--- zod/src/schemas/sdo/identity.schema.ts
+++ zod/src/schemas/sdo/identity.schema.ts
@@ -1,29 +1,32 @@
 import { z } from "zod";
 import { StixTypeSchema } from "../common/stix-type";
-import { StixIdentifierImpl } from "../../classes/stix-identifier.cls";
 import { AttackCoreSDOSchema } from "../common/core-attack-sdo.schema";
-import { StixIdentifierSchema } from "../common/stix-identifier";
+import { createStixIdentifierSchema, StixIdentifierSchema } from "../common/stix-identifier";
 
-// Custom error messages
-const IdentityError = {
-    InvalidVersion: {
-        code: z.ZodIssueCode.custom,
-        message: "Identity version must be a valid semantic version string",
-    },
-    // Add more custom error messages as needed
-};
+import '../../errors';
 
-// Identity Schema
+
+/**
+ * Determine all possible values that can exist on identity_class
+ */
+export const IdentityClassOV = z.enum(['organization']);
+
+
+/**
+ * Identity Schema
+ */
 export const IdentitySchema = AttackCoreSDOSchema.extend({
 
     id: createStixIdentifierSchema(StixTypeSchema.enum.identity),
 
+    type: z.literal(StixTypeSchema.enum.identity),
+
     object_marking_refs: z
-    .array(StixIdentifierSchema)
-    .describe("The list of marking-definition objects to be applied to this object."),
-        
-    identity_class: z.string()
-    .describe("The type of entity that this Identity describes, e.g., an individual or organization.")
+        .array(StixIdentifierSchema)
+        .describe("The list of marking-definition objects to be applied to this object."),
+
+    identity_class: IdentityClassOV
+        .describe("The type of entity that this Identity describes, e.g., an individual or organization. This is an open vocabulary and the values SHOULD come from the identity-class-ov vocabulary.")
 });
 
 // Define the type for Identity
