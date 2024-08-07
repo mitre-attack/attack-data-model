1750b96a89b6c90b85303a877b038b08644fd3f1
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2024-08-07 14:05:23 -0400

    Asset schema & examples

---
+++ zod/examples/asset.example.ts
@@ -0,0 +1,224 @@
+
+import { z } from "zod";
+import { AssetSchema } from "../src/schemas/sdo/asset.schema";
+
+/*************************************************************************************************** */
+// Example 1: Valid Asset
+/*************************************************************************************************** */
+const validAsset = {
+	"modified": "2023-10-04T18:05:43.237Z",
+	"name": "Remote Terminal Unit (RTU)",
+	"description": "A Remote Terminal Unit (RTU) is a device that typically resides between field devices (e.g., PLCs, IEDs) and control/SCADA servers and supports various communication interfacing and data aggregation functions. RTUs are typically responsible for forwarding commands from the control server and the collection of telemetry, events, and alerts from the field devices. An RTU can be implemented as a dedicated embedded device, as software platform that runs on a hardened/ruggedized computer, or using a custom application program on a PLC.",
+	"x_mitre_platforms": [
+		"Embedded",
+		"Windows",
+		"Linux"
+	],
+	"x_mitre_deprecated": false,
+	"x_mitre_domains": [
+		"ics-attack"
+	],
+	"x_mitre_version": "1.0",
+	"type": "x-mitre-asset",
+	"id": "x-mitre-asset--1769c499-55e5-462f-bab2-c39b8cd5ae32",
+	"created": "2023-09-28T14:44:54.756Z",
+	"created_by_ref": "identity--c78cb6e5-0c4b-4611-8297-d1b8b55e40b5",
+	"revoked": false,
+	"external_references": [
+		{
+			"source_name": "mitre-attack",
+			"url": "https://attack.mitre.org/assets/A0004",
+			"external_id": "A0004"
+		}
+	],
+	"object_marking_refs": [
+		"marking-definition--fa42a846-8d90-4e51-bc29-71d5b4802168"
+	],
+	"x_mitre_attack_spec_version": "3.2.0",
+	"x_mitre_modified_by_ref": "identity--c78cb6e5-0c4b-4611-8297-d1b8b55e40b5",
+	"spec_version": "2.1"
+};
+
+console.log("\nExample 1: Valid Asset:");
+console.log(`SUCCESS ${AssetSchema.parse(validAsset).name}`)
+
+/*************************************************************************************************** */
+// Example 2: Invalid Asset (ATT&CK ID does not match format A####)
+/*************************************************************************************************** */
+const invalidAssetID = {
+	...validAsset,
+	external_references: [
+		{
+			source_name: "mitre-attack",
+			external_id: "T1111"
+		}
+	]
+};
+
+console.log("\nExample 2: Invalid Asset (ATT&CK ID does not match format A####):");
+try {
+    AssetSchema.parse(invalidAssetID);
+} catch (error) {
+    if (error instanceof z.ZodError) {
+        console.log("Validation errors:", error.errors);
+    }
+}
+/**
+	Validation errors: [
+		{
+			code: 'custom',
+			message: 'The first external_reference must match the ATT&CK ID format A####.',
+			path: []
+		}
+	]
+ */
+
+/*************************************************************************************************** */
+// Example 3: Invalid Asset (missing required fields)
+/*************************************************************************************************** */
+const invalidAssetMissingFields = {
+	"modified": "2023-10-04T18:05:43.237Z",
+	// Missing name
+	"description": "A Remote Terminal Unit (RTU) is a device that typically resides between field devices (e.g., PLCs, IEDs) and control/SCADA servers and supports various communication interfacing and data aggregation functions. RTUs are typically responsible for forwarding commands from the control server and the collection of telemetry, events, and alerts from the field devices. An RTU can be implemented as a dedicated embedded device, as software platform that runs on a hardened/ruggedized computer, or using a custom application program on a PLC.",
+	"x_mitre_sectors": [
+		"Electric",
+		"Water and Wastewater",
+		"General"
+	],
+	"x_mitre_platforms": [
+		"Embedded",
+		"Windows",
+		"Linux"
+	],
+	"x_mitre_deprecated": false,
+	// Missing x_mitre_domains
+	// Missing x_mitre_version (Defaults to 1.0)
+	"type": "x-mitre-asset",
+	"id": "x-mitre-asset--1769c499-55e5-462f-bab2-c39b8cd5ae32",
+	"created": "2023-09-28T14:44:54.756Z",
+	"created_by_ref": "identity--c78cb6e5-0c4b-4611-8297-d1b8b55e40b5",
+	"revoked": false,
+	"external_references": [
+		{
+			"source_name": "mitre-attack",
+			"url": "https://attack.mitre.org/assets/A0004",
+			"external_id": "A0004"
+		}
+	],
+	"object_marking_refs": [
+		"marking-definition--fa42a846-8d90-4e51-bc29-71d5b4802168"
+	],
+	"x_mitre_attack_spec_version": "3.2.0",
+	"x_mitre_modified_by_ref": "identity--c78cb6e5-0c4b-4611-8297-d1b8b55e40b5",
+	"spec_version": "2.1"
+};
+
+console.log("\nExample 3: Invalid Asset (missing required fields):");
+try {
+    AssetSchema.parse(invalidAssetMissingFields);
+} catch (error) {
+    if (error instanceof z.ZodError) {
+        console.log("Validation errors:", error.errors);
+    }
+}
+/**
+	Validation errors: [
+		{
+			code: 'invalid_type',
+			expected: 'string',
+			received: 'undefined',
+			path: [ 'name' ],
+			message: 'Required'
+		},
+		{
+			code: 'invalid_type',
+			expected: 'array',
+			received: 'undefined',
+			path: [ 'x_mitre_domains' ],
+			message: 'Required'
+		}
+	]
+ */
+
+/*************************************************************************************************** */
+// Example 4: Asset with invalid type
+/*************************************************************************************************** */
+const assetWithInvalidType = {
+	...validAsset,
+	type: "invalid-type"
+};
+
+console.log("\nExample 4: Asset with invalid type:");
+try {
+    AssetSchema.parse(assetWithInvalidType);
+} catch (error) {
+    if (error instanceof z.ZodError) {
+        console.log("Validation error:", error.errors[0].message);
+    }
+}
+// Validation error: Invalid 'type' property. Expected 'x-mitre-asset' for Asset object, but received 'invalid-type'.
+
+/*************************************************************************************************** */
+// Example 5: Asset with optional fields
+/*************************************************************************************************** */
+const assetWithOptionalFields = {
+	...validAsset,
+	x_mitre_contributors: ["John Doe", "Jane Smith"],
+	x_mitre_sectors: [
+		"Electric",
+		"Water and Wastewater",
+		"General"
+	],
+	x_mitre_related_assets: [
+		{
+			name: "Related Asset",
+			description: "Description",
+			related_asset_sectors: ["Electric"]
+		}
+	]
+};
+
+console.log("\nExample 5: Asset with optional fields:");
+console.log(AssetSchema.parse(assetWithOptionalFields));
+
+/*************************************************************************************************** */
+// Example 6: Asset with invalid sectors
+/*************************************************************************************************** */
+const assetWithInvalidSectors = {
+	...validAsset,
+	x_mitre_sectors: [
+		"Invalid Sector"
+	]
+};
+
+console.log("\nExample 6: Asset with invalid sectors:");
+try {
+    AssetSchema.parse(assetWithInvalidSectors);
+} catch (error) {
+    if (error instanceof z.ZodError) {
+        console.log("Validation error:", error.errors[0].message);
+    }
+}
+// Validation error: Invalid enum value. Expected 'Electric' | 'Water and Wastewater' | 'Manufacturing' | 'Rail' | 'Maritime' | 'General', received 'Invalid Sector'
+
+/*************************************************************************************************** */
+// Example 7: Asset with invalid related assets
+/*************************************************************************************************** */
+const assetWithInvalidRelatedAssets = {
+	...validAsset,
+	x_mitre_related_assets: [
+		{
+			description: "invalid related asset"
+		}
+	]
+};
+
+console.log("\nExample 7: Asset with invalid related assets:");
+try {
+    AssetSchema.parse(assetWithInvalidRelatedAssets);
+} catch (error) {
+    if (error instanceof z.ZodError) {
+        console.log("Validation error:", error.errors[0].message);
+    }
+}
+// Validation error: Related asset name is required.
---
+++ zod/src/schemas/sdo/asset.schema.ts
@@ -0,0 +1,97 @@
+import { z } from 'zod';
+import { AttackCoreSDOSchema, AttackDomains, DescriptionSchema, MitreContributorsSchema, PlatformsSchema, StixIdentifierSchema, StixTypeSchema } from '../common';
+
+// Initializes the custom ZodErrorMap
+import '../../errors';
+
+export const AssetSectors = z.enum([
+	"Electric",
+	"Water and Wastewater",
+	"Manufacturing",
+	"Rail",
+	"Maritime",
+	"General"
+]);
+
+export const RelatedAssetsSchema = z.object({
+    name: z.string({
+		required_error: "Related asset name is required.",
+		invalid_type_error: "Related asset name must be a string."
+	}),
+    related_asset_sectors: z.array(AssetSectors, {
+		invalid_type_error: "related_asset_sectors must be an array."
+	}),
+});
+
+export type RelatedAssets = z.infer<typeof RelatedAssetsSchema>;
+
+export const AssetSchema = AttackCoreSDOSchema.extend({
+	type: z.literal(StixTypeSchema.enum['x-mitre-asset'], {
+		message: `'type' property must be equal to ${StixTypeSchema.enum['x-mitre-asset']}`
+	}),
+
+	description: DescriptionSchema
+		.describe("The description of the object.")
+		.optional(),
+	
+	x_mitre_platforms: PlatformsSchema,
+
+	x_mitre_domains: z
+		.array(AttackDomains)
+		.describe("The technology domains to which the ATT&CK object belongs."),
+
+	x_mitre_deprecated: z
+		.boolean({
+			invalid_type_error: "x_mitre_deprecated must be a boolean."
+		})
+		.optional(),
+
+	x_mitre_contributors: MitreContributorsSchema,
+	
+	x_mitre_sectors: z
+		.array(AssetSectors)
+		.describe("List of industry sector(s) an asset may be commonly observed in.")
+		.optional(),
+
+	x_mitre_related_assets: z
+		.array(RelatedAssetsSchema)
+		.describe("Related assets describe sector specific device names or alias that may be commonly associated with the primary asset page name or functional description. Related asset objects include a description of how the related asset is associated with the page definition.")
+		.optional(),
+
+	x_mitre_modified_by_ref: StixIdentifierSchema
+        .describe("The STIX ID of an identity object. Used to track the identity of the individual or organization which created the current version of the object. Previous versions of the object may have been created by other individuals or organizations."),
+})
+.required({
+	name: true,
+	type: true,
+	x_mitre_version: true,
+	x_mitre_domains: true,
+})
+// validate common fields
+.superRefine(({external_references}, ctx) => {
+	// ATT&CK ID format
+	if (!external_references?.length) {
+		ctx.addIssue({
+			code: z.ZodIssueCode.custom,
+			message: "At least one external_reference must be specified."
+		});
+	} else {
+		let attackIdEntry = external_references[0];
+		if (!attackIdEntry.external_id) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: "ATT&CK ID must be defined in the first external_references entry.",
+			});
+		} else {
+			let idRegex = /A\d{4}$/;
+			if (!idRegex.test(attackIdEntry.external_id)) {
+				ctx.addIssue({
+					code: z.ZodIssueCode.custom,
+					message: "The first external_reference must match the ATT&CK ID format A####."
+				})
+			}
+		}
+	}
+});
+
+export type Asset = z.infer<typeof AssetSchema>;
