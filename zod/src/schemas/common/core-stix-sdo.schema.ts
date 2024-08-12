66351eb9bcdd30df6f04716d7312afc66fd10315
Sean Sica <23294618+seansica@users.noreply.github.com>
2024-08-12 16:26:53 -0400

    fix: Update created_by_ref description in core-stix-sdo schema

--- zod/src/schemas/common/core-stix-sdo.schema.ts
+++ zod/src/schemas/common/core-stix-sdo.schema.ts
@@ -40,7 +40,7 @@ export const SDOSchema = z
             .brand("StixModifiedTimestamp")
             .describe("The modified property represents the time that this particular version of the object was modified. The timstamp value MUST be precise to the nearest millisecond."),
         created_by_ref: StixCreatedByRefSchema
-            .describe("The ID of the Source object that describes who created this object.")
+            .describe("The created_by_ref property specifies the id property of the identity object that describes the entity that created this object. If this attribute is omitted, the source of this information is undefined. This may be used by object creators who wish to remain anonymous.")
             .optional(),
         labels: z
             .array(z.string())
