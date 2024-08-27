7cbfac4d806fc61a2f5f544ce2ce41b0c99cb6e1
Sean Sica <23294618+seansica@users.noreply.github.com>
2024-08-27 14:25:07 -0400

    feat: remove default value from spec version schema and add describe method

--- zod/src/schemas/common/stix-spec-version.ts
+++ zod/src/schemas/common/stix-spec-version.ts
@@ -1,7 +1,14 @@
 import { z } from "zod";
 
+const specVersionDescription = [
+    "The version of the STIX specification used to represent this object.",
+    "The value of this property MUST be 2.1 for STIX Objects defined according to this specification.",
+    "If objects are found where this property is not present, the implicit value for all STIX Objects other than SCOs is 2.0.",
+    "Since SCOs are now top-level objects in STIX 2.1, the default value for SCOs is 2.1."
+].join(" ");
+
 export const stixSpecVersionSchema = z
     .enum(['2.0', '2.1'])
-    .default("2.1");
+    .describe(specVersionDescription);
     
 export type StixSpecVersion = z.infer<typeof stixSpecVersionSchema>;
