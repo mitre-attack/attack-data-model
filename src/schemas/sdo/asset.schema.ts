96e2dde6244591bdddb2048fc8b5c1902ad1bf68
Sean Sica <23294618+seansica@users.noreply.github.com>
2024-08-28 14:59:44 -0400

    feat(asset): finish review of mitre asset schema

--- src/schemas/sdo/asset.schema.ts
+++ src/schemas/sdo/asset.schema.ts
@@ -1,91 +1,144 @@
 import { z } from 'zod';
-import { attackBaseObjectSchema, descriptionSchema, xMitrePlatformsSchema, stixIdentifierSchema, stixTypeSchema, xMitreDomainsSchema } from '../common';
+import { attackBaseObjectSchema, descriptionSchema, xMitrePlatformsSchema, stixIdentifierSchema, stixTypeSchema, xMitreDomainsSchema, createStixIdentifierSchema, xMitreContributorsSchema, xMitreModifiedByRefSchema, objectMarkingRefsSchema, externalReferencesSchema } from '../common';
 
 // Initializes the custom ZodErrorMap
 import '../../errors';
 
-export const assetSectors = z.enum([
+
+/////////////////////////////////////
+//
+// MITRE Asset Sectors 
+// (x_mitre_sectors)
+//
+/////////////////////////////////////
+
+const supportedAssetSectors = [
 	"Electric",
 	"Water and Wastewater",
 	"Manufacturing",
 	"Rail",
 	"Maritime",
 	"General"
-]);
+] as const;
 
-export const relatedAssetsSchema = z.object({
-    name: z.string({
-		required_error: "Related asset name is required.",
-		invalid_type_error: "Related asset name must be a string."
-	}),
-	related_asset_sectors: z.array(assetSectors, {
+export const xMitreSectorsSchema = z.
+	array(
+		z.enum(supportedAssetSectors), {
 		invalid_type_error: "related_asset_sectors must be an array."
-	}),
+	})
+	.describe("List of industry sector(s) an asset may be commonly observed in.");
+
+export type XMitreSectors = z.infer<typeof xMitreSectorsSchema>;
+
+
+/////////////////////////////////////
+//
+// MITRE Related Assets
+// (x_mitre_related_assets)
+//
+/////////////////////////////////////
+
+export const relatedAssetSchema = z.object({
+
+	name: z
+		.string({
+			required_error: "Related asset name is required.",
+			invalid_type_error: "Related asset name must be a string."
+		}),
+
+	related_asset_sectors: xMitreSectorsSchema,
 });
 
+export const relatedAssetsSchema = z
+	.array(relatedAssetSchema)
+	.describe("Related assets describe sector specific device names or alias that may be commonly associated with the primary asset page name or functional description. Related asset objects include a description of how the related asset is associated with the page definition.");
+
+export type RelatedAsset = z.infer<typeof relatedAssetSchema>;
 export type RelatedAssets = z.infer<typeof relatedAssetsSchema>;
 
-export const assetSchema = attackBaseObjectSchema.extend({
-	type: z.literal(stixTypeSchema.enum['x-mitre-asset'], {
-		message: `'type' property must be equal to ${stixTypeSchema.enum['x-mitre-asset']}`
-	}),
 
-	description: descriptionSchema
-		.describe("The description of the object.")
-		.optional(),
+/////////////////////////////////////
+//
+// MITRE Asset
+//
+/////////////////////////////////////
+
+export const assetSchema = attackBaseObjectSchema.extend({
+
+	id: createStixIdentifierSchema('x-mitre-asset'),
+
+	type: z.literal(stixTypeSchema.enum['x-mitre-asset']),
+
+	description: descriptionSchema,
+
+	// Optional in STIX but required in ATT&CK
+	external_references: externalReferencesSchema,
+
+	// Optional in STIX but required in ATT&CK
+	object_marking_refs: objectMarkingRefsSchema,
 	
 	x_mitre_platforms: xMitrePlatformsSchema,
 
 	x_mitre_domains: xMitreDomainsSchema,
 
-	x_mitre_contributors: z
-        .array(z.string())
+	x_mitre_contributors: xMitreContributorsSchema
         .optional(),
 	
-	x_mitre_sectors: z
-		.array(assetSectors)
-		.describe("List of industry sector(s) an asset may be commonly observed in.")
+	x_mitre_sectors: xMitreSectorsSchema
 		.optional(),
 
-	x_mitre_related_assets: z
-		.array(relatedAssetsSchema)
-		.describe("Related assets describe sector specific device names or alias that may be commonly associated with the primary asset page name or functional description. Related asset objects include a description of how the related asset is associated with the page definition.")
+	x_mitre_related_assets: relatedAssetsSchema
 		.optional(),
 
-	x_mitre_modified_by_ref: stixIdentifierSchema
-        .describe("The STIX ID of an identity object. Used to track the identity of the individual or organization which created the current version of the object. Previous versions of the object may have been created by other individuals or organizations."),
+	x_mitre_modified_by_ref: xMitreModifiedByRefSchema,
 })
-.required({
-	name: true,
-	type: true,
-	x_mitre_version: true,
-	x_mitre_domains: true,
-})
-// validate common fields
-.superRefine(({external_references}, ctx) => {
-	// ATT&CK ID format
-	if (!external_references?.length) {
-		ctx.addIssue({
-			code: z.ZodIssueCode.custom,
-			message: "At least one external_reference must be specified."
-		});
-	} else {
-		let attackIdEntry = external_references[0];
-		if (!attackIdEntry.external_id) {
+	.required({
+		created: true,
+		created_by_ref: true,
+		description: true,
+		external_references: true,
+		id: true,
+		modified: true,
+		name: true,
+		object_marking_refs: true,
+		revoked: true,
+		spec_version: true,
+		type: true,
+		x_mitre_attack_spec_version: true,
+		x_mitre_deprecated: true,
+		x_mitre_domains: true,
+		x_mitre_modified_by_ref: true,
+		x_mitre_platforms: true,
+		x_mitre_version: true,
+	})
+	// validate common fields
+	.superRefine((schema, ctx) => {
+
+		const { external_references } = schema;
+
+		// ATT&CK ID format
+		if (!external_references?.length) {
 			ctx.addIssue({
 				code: z.ZodIssueCode.custom,
-				message: "ATT&CK ID must be defined in the first external_references entry.",
+				message: "At least one external_reference must be specified."
 			});
 		} else {
-			let idRegex = /A\d{4}$/;
-			if (!idRegex.test(attackIdEntry.external_id)) {
+			let attackIdEntry = external_references[0];
+			if (!attackIdEntry.external_id) {
 				ctx.addIssue({
 					code: z.ZodIssueCode.custom,
-					message: "The first external_reference must match the ATT&CK ID format A####."
-				})
+					message: "ATT&CK ID must be defined in the first external_references entry.",
+				});
+			} else {
+				let idRegex = /A\d{4}$/;
+				if (!idRegex.test(attackIdEntry.external_id)) {
+					ctx.addIssue({
+						code: z.ZodIssueCode.custom,
+						message: "The first external_reference must match the ATT&CK ID format A####."
+					})
+				}
 			}
 		}
-	}
-});
+	});
 
 export type Asset = z.infer<typeof assetSchema>;
