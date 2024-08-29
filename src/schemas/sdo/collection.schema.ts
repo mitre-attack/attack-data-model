bb05f4b48f541f29c12c21a6003d0cee1bfb1344
Sean Sica <23294618+seansica@users.noreply.github.com>
2024-08-29 15:55:17 -0400

    feat: complete review of collection schema

--- src/schemas/sdo/collection.schema.ts
+++ src/schemas/sdo/collection.schema.ts
@@ -1,26 +1,27 @@
 import { z } from 'zod';
-import { attackBaseObjectSchema, createStixIdentifierSchema, descriptionSchema, stixIdentifierSchema, stixTimestampSchema, stixTypeSchema } from "../common";
+import { attackBaseObjectSchema, createStixIdentifierSchema, descriptionSchema, objectMarkingRefsSchema, stixCreatedByRefSchema, stixIdentifierSchema, stixModifiedTimestampSchema, stixTypeSchema } from "../common";
 
 
 /////////////////////////////////////
 //
-// Object Version Reference (x_mitre_contents)
+// Object Version Reference 
+// (x_mitre_contents)
 //
 /////////////////////////////////////
 
 export const objectVersionReferenceSchema = z.object({
+
 	object_ref: stixIdentifierSchema
-		.refine(val => val !== undefined, {
-			message: "'object_ref' is required."
-		})
 		.describe("The ID of the referenced object."),
-	object_modified: stixTimestampSchema
-		.brand("StixModifiedTimestamp")
-		.refine(val => val !== undefined, {
-			message: "'object_modified' is required."
-		})
+
+	object_modified: stixModifiedTimestampSchema
 		.describe("The modified time of the referenced object. It MUST be an exact match for the modified time of the STIX object being referenced.")
 });
+
+export const xMitreContentsSchema = z
+	.array(objectVersionReferenceSchema)
+	.min(1, "At least one STIX object reference is required.")
+	.describe("Specifies the objects contained within the collection.")
 
 export type ObjectVersionReference = z.infer<typeof objectVersionReferenceSchema>;
 
@@ -33,31 +34,34 @@ export type ObjectVersionReference = z.infer<typeof objectVersionReferenceSchema
 
 export const collectionSchema = attackBaseObjectSchema.extend({
 
-	id: createStixIdentifierSchema(stixTypeSchema.enum['x-mitre-collection']),
+	id: createStixIdentifierSchema('x-mitre-collection'),
 
 	type: z.literal(stixTypeSchema.enum["x-mitre-collection"]),
 
-	description: descriptionSchema
-        .describe("Details, context, and explanation about the purpose or contents of the collection.")
-        .optional(),
+	// Optional in STIX but required in ATT&CK
+	created_by_ref: stixCreatedByRefSchema,
 
-	x_mitre_contents: z
-		.array(objectVersionReferenceSchema)
-		.min(1, "At least one STIX object reference is required.")
-		.describe("Specifies the objects contained within the collection."),
+	// Optional in STIX but required in ATT&CK
+	object_marking_refs: objectMarkingRefsSchema,
+
+	description: descriptionSchema
+		.describe("Details, context, and explanation about the purpose or contents of the collection."),
+
+	x_mitre_contents: xMitreContentsSchema
 })
 .required({
-	id: true,
-	type: true,
-	name: true,
 	created: true,
-	modified: true,
-	x_mitre_version: true,
-	spec_version: true,
-	x_mitre_attack_spec_version: true,
 	created_by_ref: true,
+	description: true,
+	id: true,
+	modified: true,
+	name: true,
 	object_marking_refs: true,
+	spec_version: true,
+	type: true,
+	x_mitre_attack_spec_version: true,
 	x_mitre_contents: true,
+	x_mitre_version: true,
 });
 
 export type Collection = z.infer<typeof collectionSchema>;
