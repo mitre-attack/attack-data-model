615ce91c5eb2bf38f4e339b16b8b8f74aea35a4b
Sean Sica <23294618+seansica@users.noreply.github.com>
2024-10-17 15:57:08 -0400

    chore(commitlint): update config * disable body max line length rule * ignore commits from ci bot

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,11 @@
-export default { extends: ['@commitlint/config-conventional'] };
+// commitlint.config.js
+export default {
+    extends: ['@commitlint/config-conventional'],
+    ignores: [
+        // Ignore commits starting with "chore(release):" (semantic-release commits)
+        (message) => /^chore\(release\):/s.test(message),
+    ],
+    rules: {
+        'body-max-line-length': [0, 'always'], // Disable body max line length rule
+    },
+};
