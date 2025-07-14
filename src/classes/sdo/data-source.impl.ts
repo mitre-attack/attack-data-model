afb06294a8e8924a43142d1713902e5d70f4563e
Sean Sica <23294618+seansica@users.noreply.github.com>
2025-07-14 14:33:41 -0400

    fix(data-source.impl): typo in exported interface

--- src/classes/sdo/data-source.impl.ts
+++ src/classes/sdo/data-source.impl.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-declaration-merging */
+
 import type { DataSource } from '../../schemas/sdo/data-source.schema.js';
 import { AttackBaseImpl } from '../common/attack-object.impl.js';
 
@@ -10,4 +12,6 @@ export class DataSourceImpl extends AttackBaseImpl {
 
 // Suppress the lint error for the empty interface
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface LogSourceImpl extends DataSource {}
+export interface DataSourceImpl extends DataSource {}
+
+/* eslint-enable @typescript-eslint/no-unsafe-declaration-merging */
