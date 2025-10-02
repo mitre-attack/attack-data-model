bd2c06367488a3cf9483e7bdbb35e87fe744533d
Kevin Ji <kji@mitre.org>
2025-10-02 13:22:10 -0400

    chore(docs): refactor docusaurus pages

--- docusaurus/sidebarsAPI.ts
+++ docusaurus/sidebarsAPI.ts
@@ -2,21 +2,26 @@ import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
   apiSidebar: [
+    'index',
+    'data-sources',
+    'attack-data-model',
+    'utilities',
+    'errors',
+    'configuration',
+    'common-usage-patterns',
     // {
-    //   type: 'doc',
-    //   id: 'index',
+    //   type: 'category',
     //   label: 'API Documentation',
+    //   link: { type: 'doc', id: 'index' },
+    //   items: [
+    //     'data-sources',
+    //     'attack-data-model',
+    //     'utilities',
+    //     'errors',
+    //     'configuration',
+    //     'common-usage-patterns',
+    //   ],
     // },
-    {
-      type: 'category',
-      label: 'API Documentation',
-      link: { type: 'doc', id: 'index' },
-      items: [
-        'data-sources',
-        'attack-data-model',
-        'utilities',
-      ],
-    },
   ],
 };
 
