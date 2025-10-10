dbb7f0d1c6bb88c7f2d99377401780f998a44697
Jared Ondricek <jondricek@mitre.org>
2025-10-10 10:35:18 -0500

    docs: remove deprecated docusaurus config item

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -19,7 +19,6 @@ const config: Config = {
   projectName: 'attack-data-model', // Usually your repo name.
 
   onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
