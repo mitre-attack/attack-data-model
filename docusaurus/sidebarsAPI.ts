cfdc98043f30c2b9348fbb2aaa4defb9803efb36
Jared Ondricek <jondricek@mitre.org>
2025-10-15 22:49:45 -0500

    docs: remove unwanted content from API section

--- docusaurus/sidebarsAPI.ts
+++ docusaurus/sidebarsAPI.ts
@@ -3,13 +3,11 @@ import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
 const sidebars: SidebarsConfig = {
   apiSidebar: [
     'index',
-    'data-sources',
-    'attack-data-model',
-    'utilities',
-    'errors',
-    'configuration',
-    'common-usage-patterns',
-    'changelog-api',
+    {
+      type: 'link',
+      label: 'Changelog',
+      href: 'https://github.com/mitre-attack/attack-data-model/releases'
+    }
   ],
 };
 
