3837306d6ee55a671e62e863edf64418eb8f5139
Kevin Ji <kji@mitre.org>
2025-12-15 10:51:37 -0500

    docs: add description to software schema

--- src/schemas/sdo/software.schema.ts
+++ src/schemas/sdo/software.schema.ts
@@ -18,36 +18,41 @@ import {
 //
 //==============================================================================
 
-export const softwareSchema = attackBaseDomainObjectSchema.extend({
-  type: createMultiStixTypeValidator(['malware', 'tool']),
+export const softwareSchema = attackBaseDomainObjectSchema
+  .extend({
+    type: createMultiStixTypeValidator(['malware', 'tool']),
 
-  created_by_ref: stixCreatedByRefSchema.meta({
-    description: 'The ID of the Source object that describes who created this object.',
-  }),
+    created_by_ref: stixCreatedByRefSchema.meta({
+      description: 'The ID of the Source object that describes who created this object.',
+    }),
 
-  description: descriptionSchema,
+    description: descriptionSchema,
 
-  external_references: externalReferencesSchema,
+    external_references: externalReferencesSchema,
 
-  // Malware: Required
-  // Tool: Optional
-  x_mitre_platforms: xMitrePlatformsSchema.optional(),
+    // Malware: Required
+    // Tool: Optional
+    x_mitre_platforms: xMitrePlatformsSchema.optional(),
 
-  x_mitre_contributors: xMitreContributorsSchema.optional(),
+    x_mitre_contributors: xMitreContributorsSchema.optional(),
 
-  x_mitre_aliases: aliasesSchema.optional().meta({
+    x_mitre_aliases: aliasesSchema.optional().meta({
+      description:
+        "Alternative names used to identify this software. The first alias must match the object's name.",
+    }),
+
+    x_mitre_modified_by_ref: xMitreModifiedByRefSchema,
+
+    x_mitre_domains: xMitreDomainsSchema,
+
+    // Not used in ATT&CK Malware or Tool but defined in STIX
+    aliases: aliasesSchema
+      .optional()
+      .meta({ description: 'Alternative names used to identify this software.' }),
+  })
+  .meta({
     description:
-      "Alternative names used to identify this software. The first alias must match the object's name.",
-  }),
-
-  x_mitre_modified_by_ref: xMitreModifiedByRefSchema,
-
-  x_mitre_domains: xMitreDomainsSchema,
-
-  // Not used in ATT&CK Malware or Tool but defined in STIX
-  aliases: aliasesSchema
-    .optional()
-    .meta({ description: 'Alternative names used to identify this software.' }),
-});
+      'Software represents tools and malicious code used by adversaries to accomplish their objectives. ATT&CK models software using two STIX object types: [malware](http://docs.oasis-open.org/cti/stix/v2.0/csprd01/part2-stix-objects/stix-v2.0-csprd01-part2-stix-objects.html#_Toc476230945) and [tool](http://docs.oasis-open.org/cti/stix/v2.0/csprd01/part2-stix-objects/stix-v2.0-csprd01-part2-stix-objects.html#_Toc476230961).',
+  });
 
 export type Software = z.infer<typeof softwareSchema>;
