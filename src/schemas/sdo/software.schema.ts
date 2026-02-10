604a8d6c7befedf77b39fd9e47b4d489d684e1e3
Sean Sica <23294618+seansica@users.noreply.github.com>
2026-02-10 13:40:57 -0500

    style: apply formatting

--- src/schemas/sdo/software.schema.ts
+++ src/schemas/sdo/software.schema.ts
@@ -8,11 +8,9 @@ import { toolSchema } from './tool.schema.js';
 //
 //==============================================================================
 
-export const softwareSchema = z
-  .union([malwareSchema, toolSchema])
-  .meta({
-    description:
-      'Software represents tools and malicious code used by adversaries to accomplish their objectives. ATT&CK models software using two STIX object types: [malware](http://docs.oasis-open.org/cti/stix/v2.0/csprd01/part2-stix-objects/stix-v2.0-csprd01-part2-stix-objects.html#_Toc476230945) and [tool](http://docs.oasis-open.org/cti/stix/v2.0/csprd01/part2-stix-objects/stix-v2.0-csprd01-part2-stix-objects.html#_Toc476230961).',
-  });
+export const softwareSchema = z.union([malwareSchema, toolSchema]).meta({
+  description:
+    'Software represents tools and malicious code used by adversaries to accomplish their objectives. ATT&CK models software using two STIX object types: [malware](http://docs.oasis-open.org/cti/stix/v2.0/csprd01/part2-stix-objects/stix-v2.0-csprd01-part2-stix-objects.html#_Toc476230945) and [tool](http://docs.oasis-open.org/cti/stix/v2.0/csprd01/part2-stix-objects/stix-v2.0-csprd01-part2-stix-objects.html#_Toc476230961).',
+});
 
 export type Software = z.infer<typeof softwareSchema>;
